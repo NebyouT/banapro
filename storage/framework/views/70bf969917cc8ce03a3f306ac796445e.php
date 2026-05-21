88ea98a72eeda0cb0f996cbc96434170c8656388
NebyouT <nebyoutilikalem@gmail.com>
2026-05-21 19:27:41 +0300

    busness module added

--- storage/framework/views/70bf969917cc8ce03a3f306ac796445e.php
+++
@@ -1,5 +0,0 @@
-<footer class="container-fluid d-flex align-items-center justify-content-center justify-content-sm-between flex-wrap py-3 ms-0 bg-white">
-    <p class="mb-0 me-3"> <?php echo e(get_option('general')['copy_right'] ?? ''); ?></p>
-    <p class="mb-0"><?php echo e(get_option('general')['admin_footer_text'] ?? ''); ?>: <a class="footer-acn" href="<?php echo e(get_option('general')['admin_footer_link'] ?? ''); ?>" target="_blank"><?php echo e(get_option('general')['admin_footer_link_text'] ?? ''); ?></a></p>
-</footer>
-<?php /**PATH /home/banaeaou/pro.banaeats.com/resources/views/layouts/partials/footer.blade.php ENDPATH**/ ?>
